1c5fd61ebaa989d8ab56f14d90da146cba87ed47
Ankur Gupta <103580256+ankurgupta9@users.noreply.github.com>
2023-09-25 18:24:28 +0530

    Time: 2 ms (70.52%), Space: 7 MB (8.68%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+    int ans=0;
+    for (int i = 0; i < s.length(); i++)
+    {
+        ans -= s[i];
+        ans += t[i];
+    }
+    ans += t[s.length()];
+    return char(ans);
+   }
+};
